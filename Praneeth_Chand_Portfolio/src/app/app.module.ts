37c02e3fad7e6d7c4a05802daf1ed78e4b944dd9
nani6565chand <Praneethchand99@gmail.com>
2025-04-02 18:22:12 -0400

    Added Changes on icons and contact links

--- Praneeth_Chand_Portfolio/src/app/app.module.ts
+++ Praneeth_Chand_Portfolio/src/app/app.module.ts
@@ -9,6 +9,7 @@ import { NavbarComponent } from './components/navbar/navbar.component';
 import { ExperienceComponent } from './components/experience/experience.component';
 import { ProjectsComponent } from './components/projects/projects.component';
 import { ContactComponent } from './components/contact/contact.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
@@ -18,7 +19,8 @@ import { ContactComponent } from './components/contact/contact.component';
     NavbarComponent,
     ExperienceComponent,
     ProjectsComponent,
-    ContactComponent
+    ContactComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
