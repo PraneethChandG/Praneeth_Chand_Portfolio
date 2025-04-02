6b19c9e8c59cdd2a1601b46159ac6d3389a5fdd7
nani6565chand <Praneethchand99@gmail.com>
2025-04-02 13:27:20 -0400

    Navbar updated

--- Praneeth_Chand_Portfolio/src/app/app.module.ts
+++ Praneeth_Chand_Portfolio/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeroComponent } from './components/hero/hero.component';
 import { AboutComponent } from './components/about/about.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeroComponent,
-    AboutComponent
+    AboutComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
